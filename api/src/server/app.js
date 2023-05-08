38d06b347f7479ec78c1f0afa7a1410eeb2a1b05
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2023-05-08 16:02:47 -0300

    He agregado los permisos al servidor

--- api/src/server/app.js
+++ api/src/server/app.js
@@ -11,6 +11,18 @@ server.use(morgan("dev"));
 server.use(cors());
 server.use(express.json());
 
+const local = "http://localhost:3000"
+const versel = "https://pi-dogs-mf.vercel.app"
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', local); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 
 
 server.use("/",router);
