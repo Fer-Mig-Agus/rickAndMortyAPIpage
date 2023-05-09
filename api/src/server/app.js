02d9b045d81497b702cdb785081b6670981d184f
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2023-05-09 16:00:25 -0300

    Todo el deploy listo

--- api/src/server/app.js
+++ api/src/server/app.js
@@ -1,10 +1,10 @@
-const express=require("express");
-const morgan=require("morgan");
-const cors=require("cors");
-const router=require("../router/index");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const router = require("../router/index");
 
 
-const server=express();
+const server = express();
 
 
 server.use(morgan("dev"));
@@ -14,11 +14,11 @@ server.use(express.json());
 
 
 
-const local = "http://localhost:3000"
-const versel = "https://pi-dogs-mf.vercel.app"
+const desarrolloApp = "http://localhost:3000";
+const produccionApp = "https://rickandmorty-mf.vercel.app"
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', local); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', produccionApp); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -28,10 +28,10 @@ server.use((req, res, next) => {
 
 
 
-server.use("/",router);
+server.use("/", router);
 
 
-module.exports=server;
+module.exports = server;
 
 
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {Routes,Route, useLocation} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import DetailPage from "./views/DetailPage";
 import FavoritePage from "./views/FavoritesPage";
@@ -15,21 +15,22 @@ import Nav from './components/Nav';
 //important: Esta configuracion de axios es para el deploy
 import axios from "axios";
 
-const desarrolloApp ="http://localhost:3001";
-const produccionApp="";
-axios.defaults.baseURL=desarrolloApp;
+const desarrolloApp = "http://localhost:3001";
+const produccionApp = "https://rickandmortyapipage-production.up.railway.app";
+
+axios.defaults.baseURL = produccionApp;
 
 function App() {
 
-  const {pathname}=useLocation();
+  const { pathname } = useLocation();
 
-  const verificarRutas=()=>{
-    switch(pathname){
+  const verificarRutas = () => {
+    switch (pathname) {
       case "/": return false
       case "/home": return true
       case "/about": return true
-      case "/favorite":return true
-      case "/detail/:id":return false
+      case "/favorite": return true
+      case "/detail/:id": return false
       case "/register": return false
       default: return false
     }
@@ -42,16 +43,16 @@ function App() {
     <div className="App">
 
       {verificarRutas() && <Nav />}
-      
+
       <Routes>
 
-        <Route path="/" element={< LeandingPage/>}/>
-        <Route path="/home" element={< HomePage/>}/>
+        <Route path="/" element={< LeandingPage />} />
+        <Route path="/home" element={< HomePage />} />
         <Route path="/favorite" element={< FavoritePage />} />
         <Route path="/about" element={< AboutPage />} />
         <Route path="/detail/:id" element={< DetailPage />} />
-        <Route path='/register' element={<Register />}/>
-        <Route path='*' element={<ErrorPage />}/>
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<ErrorPage />} />
 
       </Routes>
 
